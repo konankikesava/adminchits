a3c6751608c414ddbe96867b782b36fa8b714bac
konankikesava <konankik@hotmail.com>
2020-12-29 14:26:29 +0200

    update2

--- src/app/add-employee/add-employee.component.spec.ts
+++ src/app/add-employee/add-employee.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AddEmployeeComponent } from './add-employee.component';
 
@@ -6,7 +6,7 @@ describe('AddEmployeeComponent', () => {
   let component: AddEmployeeComponent;
   let fixture: ComponentFixture<AddEmployeeComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AddEmployeeComponent ]
     })
